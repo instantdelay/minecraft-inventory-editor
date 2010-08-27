a8c23586a747f2339335beef21302eabf8d03df2
Spencer Van Hoose <spencer@instantdelay.com>
2010-08-26 22:34:25 -0700

    test commit

--- src/com/instantdelay/mpie/AppDriver.java
+++ src/com/instantdelay/mpie/AppDriver.java
@@ -1,10 +1,22 @@
 package com.instantdelay.mpie;
 
-public class AppDriver {
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+public class AppDriver implements Runnable {
 
    public static void main(String[] args) {
-      // TODO Auto-generated method stub
+      SwingUtilities.invokeLater(new AppDriver());
+   }
 
+   @Override
+   public void run() {
+      JFrame mainWindow = new JFrame("Minecraft Inventory Editor");
+      mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      
+      mainWindow.setSize(500, 350);
+      mainWindow.setLocationRelativeTo(null);
+      mainWindow.setVisible(true);
    }
 
 }
---
+++ src/com/instantdelay/mpie/InventoryItem.java
@@ -0,0 +1,19 @@
+package com.instantdelay.mpie;
+
+import org.jnbt.CompoundTag;
+
+/**
+ * Represents an inventory item.
+ * 
+ * @author Spencer Van Hoose
+ *
+ */
+public class InventoryItem {
+   
+   private CompoundTag itemData;
+   
+   public InventoryItem() {
+      // TODO Auto-generated constructor stub
+   }
+   
+}
